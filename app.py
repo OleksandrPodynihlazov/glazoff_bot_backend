4722827e36fb090c7a3c6aaad56b9c3cf2cddbd5
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2025-01-21 10:49:51 +0000

    update telegram bot interconnection. add image row to db

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-DB_PATH ="/opt/render/project/src/business.db"
+DB_PATH ="business.db"
 
 
 @app.route("/services",methods=["GET"])
@@ -23,6 +23,7 @@ def get_services():
                 "service_name":row[2],
                 "service_p":row[3],
                 "service_price":row[4],
+                "service_image_url":row[5]
             }
             for row in rows
         ]
@@ -33,5 +34,29 @@ def get_services():
         return jsonify(services)
     except Exception as e:
         return jsonify({"error":str(e)}),500
+    
+    
+@app.route('/order', methods=['POST'])
+def create_order():
+    data = request.json
+    service_id = data.get('service_id')
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    details = data.get('details')
+
+    # Підключення до бази даних
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO orders (service_id, name, email, phone, details)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (service_id, name, email, phone, details))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Order successfully created"}), 200
 if __name__ == "__main__":
     app.run(debug=True)
